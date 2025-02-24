83888fbb66c9fc49f306186bdf3cd3a11cf6051c
lixuejian <1305337942@qq.com>
2025-02-24 17:36:13 +0800

    允许来自所有的域名请求

--- app.js
+++ app.js
@@ -18,8 +18,10 @@ app.use(static(path.join(__dirname, 'uploads')));
 
 // 添加cors中间件配置
 app.use(cors({
-  origin: ['http://localhost:8866'],
-  maxAge: 5, // 指定本次预检请求的有效期，单位为秒 。
+  origin: function(ctx) { // 设置允许来自指定域名请求
+    return '*'; // 允许来自所有域名请求
+  },
+  maxAge: 5, // 指定本次预检请求的有效期，单位为秒。
   credentials: true, // 是否允许发送Cookie
   allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // 设置所允许的HTTP请求方法
   allowHeaders: ['Content-Type', 'Authorization', 'Accept'], // 设置服务器支持的所有头信息字段
